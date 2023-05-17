4486ddca598ff470c0a7e66f5abdbbf1c268fb21
Soufiane Amini <soufianeamini2002@gmail.com>
2023-05-17 10:13:49 +0100

    added struct for command for parsing, and addded quotes in lexer

--- src/main.rs
+++ src/main.rs
@@ -18,6 +18,25 @@ struct Token {
     ttype: TokenType,
 }
 
+struct Command {
+    cmd: String,
+    args: Vec<String>,//subject to change depending on what type the std::Command.args takes as
+    //arguments
+    infiles: Vec<String>,
+    outfiles: Vec<String>,
+}
+
+impl Command {
+    fn new(cmd: String, args: Vec<String>, infiles: Vec<String>, outfiles: Vec<String>) -> Command {
+        Command {
+            cmd,
+            args,
+            infiles,
+            outfiles,
+        }
+    }
+}
+
 impl Token {
     fn new(value: String, ttype: TokenType) -> Token {
         Token {
@@ -31,8 +50,29 @@ fn lexer(line: &String) -> Vec<Token> {
     let mut list: Vec<Token> = Vec::new();
     let mut value = String::new();
     let mut prevc = ' ';
+    let mut doublequotes = false;
+    let mut singlequotes = false;
     for c in line.trim().chars() {
+        if doublequotes == true {
+            if c == '"' {
+                doublequotes = false;
+                //If you use this logic, you would have to expand variables inside the lexer...
+            } else {
+                value.push(c);
+            }
+            continue;
+        }
+        if singlequotes == true {
+            if c == '\'' {
+                singlequotes = false;
+            } else {
+                value.push(c);
+            }
+            continue;
+        }
         match c {
+            '"' => doublequotes = true,
+            '\'' => singlequotes = true,
             '|' => {
                 if !value.is_empty() {
                     list.push(Token::new(value, TokenType::WORD));
@@ -106,11 +146,13 @@ fn lexer(line: &String) -> Vec<Token> {
     if !value.is_empty() {
         list.push(Token::new(value, TokenType::WORD));
     }
-    if !list.is_empty() {
-        if list.last().unwrap().ttype != TokenType::WORD {
-            unexpected_token(&list.last().unwrap().value);
-            return Vec::new();
-        }
+    if !list.is_empty() && list.last().unwrap().ttype != TokenType::WORD {
+        unexpected_token(&list.last().unwrap().value);
+        return Vec::new();
+    }
+    if doublequotes == true {
+        println!("Error: unclosed quotes");
+        return Vec::new();
     }
     list
 }
@@ -118,6 +160,39 @@ fn lexer(line: &String) -> Vec<Token> {
 fn unexpected_token(s:&String) {
     println!("Error: syntax error near unexpected token `{}'", s);
 }
+
+fn print_tokens(list: &Vec<Token>) {
+    for token in list {
+        print!("Token: {} -- Type: ", token.value);
+        match token.ttype {
+            TokenType::WORD => println!("Word"),
+            TokenType::PIPE => println!("Pipe"),
+            // TokenType::AND => println!("And"),
+            TokenType::OR => println!("Or"),
+            // TokenType::ERROR => println!("Error"),
+            TokenType::LESS => println!("Input Redirection"),
+            TokenType::GREAT => println!("Output Redirection - Truncate"),
+            TokenType::LESSLESS => println!("Heredoc"),
+            TokenType::GREATGREAT => println!("Output Redirection - Append"),
+        }
+    }
+}
+
+// fn parser(list: &Vec<Token>) {
+//     let mut cmd = String::new();
+//     let mut args:Vec<String> = Vec::new();
+//     for word in list {
+//         match word.ttype {
+//             TokenType::WORD => {
+//                 if cmd.is_empty() {
+//                     cmd.clone_from(&word.value);
+//                 } else {
+//                     args.push(word.value.clone());
+//                 }
+//             }
+//         }
+//     }
+// }
 
 fn main() {
     loop {
@@ -133,20 +208,8 @@ fn main() {
             return;
         }
         let list = lexer(&line);
-
-        for token in list {
-            print!("Token: {} -- Type: ", token.value);
-            match token.ttype {
-                TokenType::WORD => println!("Word"),
-                TokenType::PIPE => println!("Pipe"),
-                // TokenType::AND => println!("And"),
-                TokenType::OR => println!("Or"),
-                // TokenType::ERROR => println!("Error"),
-                TokenType::LESS => println!("Input Redirection"),
-                TokenType::GREAT => println!("Output Redirection - Truncate"),
-                TokenType::LESSLESS => println!("Heredoc"),
-                TokenType::GREATGREAT => println!("Output Redirection - Append"),
-            }
-        }
+        print_tokens(&list);//Try adding handling of quotes now, before going to parsing (which
+        //handles env variables)
+        // parser(&list);
     }
 }
