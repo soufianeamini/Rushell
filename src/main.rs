edd40fd3abe03299e7b47c11f9553ee972197b15
samini <soufianeamini2002@gmail.com>
2023-09-21 21:41:56 +0100

    added rustyline, as well as handling interrupt/eof

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use rustyline::DefaultEditor;
+
 fn main() {
-    unimplemented!();
+    let mut rl = DefaultEditor::new().unwrap();
+    loop {
+        let line = rl.readline("> ");
+        match line {
+            Ok(line) => println!("{line}"),
+            Err(e) => {
+                println!("{}", e.to_string());
+                std::process::exit(1)
+            }
+        }
+    }
 }
