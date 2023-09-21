2541af4bb0451f1725a829d0690b48d7c7707e6d
samini <soufianeamini2002@gmail.com>
2023-09-21 22:07:09 +0100

    added a few more tests

--- src/lib.rs
+++ src/lib.rs
@@ -43,7 +43,7 @@ mod lexer {
 }
 
 #[cfg(test)]
-mod tests {
+mod lexer_tests {
     use crate::lexer::TokenType;
 
     use super::*;
@@ -51,6 +51,30 @@ mod tests {
     #[test]
     fn words_only() {
         let line = "echo ls";
+
+        let tokens = lexer::lex(line);
+
+        assert_eq!(tokens[0].literal, "echo");
+        assert_eq!(tokens[0].ttype, TokenType::WORD);
+        assert_eq!(tokens[1].literal, "ls");
+        assert_eq!(tokens[1].ttype, TokenType::WORD);
+    }
+
+    #[test]
+    fn words_more_than_one_space() {
+        let line = "echo       ls";
+
+        let tokens = lexer::lex(line);
+
+        assert_eq!(tokens[0].literal, "echo");
+        assert_eq!(tokens[0].ttype, TokenType::WORD);
+        assert_eq!(tokens[1].literal, "ls");
+        assert_eq!(tokens[1].ttype, TokenType::WORD);
+    }
+
+    #[test]
+    fn words_trailing_leading_spaces() {
+        let line = "    echo ls   ";
 
         let tokens = lexer::lex(line);
 
