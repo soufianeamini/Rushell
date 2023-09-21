47391b9be204297e934af049e0f673fdd3483c94
samini <soufianeamini2002@gmail.com>
2023-09-21 22:56:47 +0100

    exported lexer to lexer.rs

---
+++ src/lexer.rs
@@ -0,0 +1,39 @@
+pub fn lex(line: &str) -> Vec<Token> {
+    let mut tokens: Vec<Token> = Vec::new();
+    let mut it = line.chars().into_iter().peekable();
+    let mut value = String::new();
+
+    while let Some(char) = it.next() {
+        match char {
+            ' ' => push_word(&mut value, &mut tokens),
+            _ => value.push(char),
+        }
+    }
+
+    push_word(&mut value, &mut tokens);
+    tokens
+}
+
+#[derive(PartialEq, Debug)]
+pub enum TokenType {
+    WORD,
+    PIPE,
+}
+
+#[derive(Debug)]
+pub struct Token {
+    pub literal: String,
+    pub ttype: TokenType,
+}
+
+fn push_word(value: &mut String, tokens: &mut Vec<Token>) {
+    if !value.is_empty() {
+        tokens.push(Token {
+            literal: value.clone(),
+            ttype: TokenType::WORD,
+        })
+    }
+
+    value.clear()
+}
+
--- src/lib.rs
+++ src/lib.rs
@@ -1,43 +1,4 @@
-pub mod lexer {
-    #[derive(PartialEq, Debug)]
-    pub enum TokenType {
-        WORD,
-        PIPE,
-    }
-
-    #[derive(Debug)]
-    pub struct Token {
-        pub literal: String,
-        pub ttype: TokenType,
-    }
-
-    fn push_word(value: &mut String, tokens: &mut Vec<Token>) {
-        if !value.is_empty() {
-            tokens.push(Token {
-                literal: value.clone(),
-                ttype: TokenType::WORD,
-            })
-        }
-
-        value.clear()
-    }
-
-    pub fn lex(line: &str) -> Vec<Token> {
-        let mut tokens: Vec<Token> = Vec::new();
-        let mut it = line.chars().into_iter().peekable();
-        let mut value = String::new();
-
-        while let Some(char) = it.next() {
-            match char {
-                ' ' => push_word(&mut value, &mut tokens),
-                _ => value.push(char),
-            }
-        }
-
-        push_word(&mut value, &mut tokens);
-        tokens
-    }
-}
+pub mod lexer;
 
 #[cfg(test)]
 mod lexer_tests {
