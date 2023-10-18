45c2696adf517f9dc85228b175e96d165e4f1b2d
samini <soufianeamini2002@gmail.com>
2023-10-18 20:54:22 +0100

    wrote something that might work nicely

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,3 +1,5 @@
+use std::{iter::Peekable, str::Chars};
+
 use TokenType::*;
 
 pub fn lex(line: &str) -> Vec<Token> {
@@ -27,6 +29,10 @@ pub fn lex(line: &str) -> Vec<Token> {
     tokens
 }
 
+// Ok so the way to refactor this, is to make a struct that contains the "happy" path for token
+// and the repeat token names, and then you'd pass that in to the function, as well ass the char to
+// check for, and the &mut value and &mut tokens obviously
+
 #[derive(PartialEq, Debug)]
 pub enum TokenType {
     WORD,
@@ -49,6 +55,33 @@ impl Token {
             literal: String::from(literal),
             ttype,
         }
+    }
+}
+
+struct LexerOpt {
+    single_literal: String,
+    single_type: TokenType,
+    repeat_literal: String,
+    repeat_type: TokenType,
+}
+
+impl LexerOpt {
+    fn new(l1: &str, t1: TokenType, l2: &str, t2: TokenType) -> LexerOpt {
+        LexerOpt {
+            single_literal: String::from(l1),
+            single_type: t1,
+            repeat_literal: String::from(l2),
+            repeat_type: t2,
+        }
+    }
+}
+
+fn generate_repeatable_token(opt: LexerOpt, char_check: char, it: &mut Peekable<Chars> ,value: &mut String, tokens: &mut Vec<Token>) {
+    if it.peek().is_some() && *it.peek().unwrap() == char_check {
+        generate_token(&opt.repeat_literal, opt.repeat_type, value, tokens);
+        it.next();
+    } else {
+        generate_token(&opt.single_literal, opt.single_type, value, tokens);
     }
 }
 
